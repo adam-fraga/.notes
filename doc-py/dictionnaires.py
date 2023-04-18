c5f24839e34997891708573aebebc5f22e84dde0
ad-mindset <contact@ad-mindset.com>
2023-04-18 11:09:48 +0200

    Update dic

--- doc-py/dictionnaires.py
+++ doc-py/dictionnaires.py
@@ -1,10 +1,8 @@
 """
 Dictionnaires (Equivalent aux tableau associatif paire clés valeurs
-Pour accéder au valeur d'un dictionnaire on utilise cette syntaxe:
-mon_dictionnaire[clés_de_mon_dictionnaire]
 """
 
-# Methode 1
+# Cration Methode 1
 nouvelle_campagne = {
     "responsable_de_campagne": "Jeanne d'Arc",
     "nom_de_campagne": "Campagne nous aimons les chiens",
@@ -12,8 +10,41 @@ nouvelle_campagne = {
     "influenceurs_importants": ["@AmourDeChien", "@MeilleuresFriandises"],
 }
 
-# Methode 2
+# Creation Methode 2
 taux_de_conversion = {}
 taux_de_conversion["facebook"] = 3.4
 taux_de_conversion["instagram"] = 1.2
 taux_de_conversion = dict()
+
+# Interpolation (Variable dynamique) possible pour les clés & valeur
+company = "CIMPA"
+project = "3D Jump"
+
+my_dict = {
+    "company": "{company}",
+    "project": "{project}",
+    "members": ["Marina, Abde, Marie"],
+}
+
+key_interpolation = my_dict["company"].format(company=company)
+value_interpolation = my_dict["film"].format(project=project)
+
+# Supprimer une valeur
+del my_dict["company"]
+
+# Accéder au valeurs
+nouvelle_campagne["responsable_de_campagne"]
+
+# Check if in dict
+if "project" in my_dict:
+    print("The project key exists in the dictionary")
+else:
+    print("The project key does not exist in the dictionary")
+
+# Access All keys and valus in Dic
+print(my_dict.keys())
+print(my_dict.values())
+
+# Iterer sur dic
+for key, value in my_dict.items():
+    print(key, value)
